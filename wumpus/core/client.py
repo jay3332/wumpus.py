48c9ee39b1896a44059f00c70293f9a9376d934c
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-07-08 05:44:10 +0000

    Emitter (base) and new type aliases

--- wumpus/core/client.py
+++ wumpus/core/client.py
@@ -1,13 +1,53 @@
 from asyncio import get_event_loop, AbstractEventLoop
-from ..typings.core import HTTPVersion, GatewayVersion
+from typing import Dict, List, NamedTuple, Union, overload
+
+from ..typings.core import HTTPVersion, GatewayVersion, EmitterCallback
 
 
 __all__ = (
     'Client',
+    'Emitter'
 )
 
 
-class Client:
+class WeakListener(typing.NamedTuple):
+    event: str
+    callback: EmitterCallback
+    count: int = None 
+
+
+class Emitter:
+    def __init__(self):
+        self.__direct_listeners: Dict[str, EmitterCallback]
+        self.__listeners: List[WeakListener] = []
+
+    def _add_direct_listener(self, event: str, callback: EmitterCallback) -> None:
+        self.__direct_listeners[event] = callback
+
+    @overload
+    def event(self, event: str) -> Callable[[EmitterCallback], EmitterCallback]:
+        ...
+
+    @overload
+    def event(self, callback: EmitterCallback) -> EmitterCallback:
+        ...
+
+    def event(self, event: Union[str, EmitterCallback]):  # We will overload this
+        if isinstance(event, str):
+            def decorator(callback: EmitterCallback):
+                self._add_direct_listener(event, callback)
+                return callback
+
+            return decorator
+        
+        name = event.__name__
+        if not name.startswith('on_'):
+            raise ValueError('event callback functions must start with "on_"')
+
+        self._add_direct_listener(name[3:], event)
+
+
+class Client(Emitter):
     """
     Represents a client connection to Discord.
     """
@@ -21,6 +61,7 @@ class Client:
         gateway_version: GatewayVersion = 9,
         loop: AbstractEventLoop = None
     ) -> None:
+        super().__init__()
         self._loop: AbstractEventLoop = loop or get_event_loop() 
         
 
--- wumpus/typings/core.py
+++ wumpus/typings/core.py
@@ -1,19 +1,20 @@
-from typing import Literal
-
-
-
-from typing import Any, Dict, Literal, Tuple
+from typing import Any, Awaitable, Callable, Dict, Literal, Tuple, Union
 
 
 __all__: Tuple[str, ...] = (
     'JSON',
+    'Snowflake',
     'HTTPVersion',
     'GatewayVersion',
     'HTTPRequestMethod'
 )
 
 
+# Common
 JSON = Dict[str, Any]  # This may be changed in the future
+Snowflake = int  # Type alias for readability
+
+EmitterCallback = Callable[[Any, ...], Union[Awaitable[Any], Any]]
 
 HTTPVersion = Literal[3, 4, 5, 6, 7, 8, 9]
 GatewayVersion = Literal[4, 5, 6, 7, 8, 9]
