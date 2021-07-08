77d633db42287e6cf6e7baefd1f756ae9f40d244
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-07-08 13:46:34 -0700

    HTTP request function

--- wumpus/core/http.py
+++ wumpus/core/http.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-from aiohttp import ClientSession
+import asyncio
 from typing import Any, Awaitable, Dict, Optional, TypeVar
+import datetime
 
-from ..typings.core import HTTPRequestMethod, JSON
+from aiohttp import ClientSession
+
+from ..typings.core import JSON, HTTPRequestMethod
+from .connection import Connection
 
 RT = TypeVar('RT', bound='Router')
 
@@ -99,11 +103,15 @@ class Router:
 
 
 class HTTPClient:
-    __slots__ = ('__session', '__api_router')
+    MAX_RETRIES = 3
+    __slots__ = ('__session', '__api_router', '_global_ratelimited', '_buckets_lock')
 
-    def __init__(self, /, *, v: int = 9):
+    def __init__(self, v: int = 9, connection: Connection, /):
         self.__session: ClientSession = ClientSession()
+        self._connection = connection
         self.__api_router: Router = Router(base=f'https://discord.com/api/v{v}', http=self)
+        self._global_ratelimited = asyncio.Event()
+        self._buckets_lock = {}
 
     @property
     def api(self) -> Router:
@@ -116,9 +124,39 @@ class HTTPClient:
         /,
         *,
         params: Dict[str, Any],
-        data: JSON
+        data: JSON,
+        headers: Dict[str, str]
     ) -> Optional[JSON]:
-        ...
+        bucket_key = f"{method} {route}"
+        bucket = self._buckets_lock.get(bucket_key)
+        if bucket is None:
+            self._buckets_lock[bucket_key] = bucket = asyncio.Lock()
+        _headers = {'Authorization': self._connection.token}
+        _headers['Content-Type'] = 'application/json'
+        _headers.update(headers)
+        await bucket.acquire()
+        if not self._global_ratelimited.is_set():
+            await self._global_ratelimited.wait()
+        for tries in range(self.MAX_RETRIES):
+            async with self.__session.request(method, route, params=params, js=data, headers=_headers) as resp:
+                bucket_remianing = resp.headers.get("X-RateLimit-Remaining")
+                data = await resp.json()
+                if int(bucket_remianing) == 0:
+                    await asyncio.sleep(float(resp.headers.get("X-RateLimit-Reset-After")))
+            if 300 > response.status >= 200:
+                return data
+            if resp.status == 429:
+                is_global = data.get("global", False)
+                if is_global is True:
+                    self._global_ratelimited.clear()
+                await asyncio.sleep(float(data.get("retry_after")))
+                if is_global:
+                    self._global_ratelimited.set()
+                continue
+            
+            # TODO: Handle all 40x status code and 50x status code
+        
+
 
     async def close(self) -> None:
         await self.__session.close()
