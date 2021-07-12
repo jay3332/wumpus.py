2a10c4a9ced6fc7351548599215af3f2a5cb751f
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-07-12 15:39:16 -0700

    Import ValidDeleteMessageDays from typings

--- wumpus/models/member.py
+++ wumpus/models/member.py
@@ -2,14 +2,13 @@ from datetime import datetime
 from typing import Optional, List, Literal
 
 from ..core.connection import Connection
-from ..typings import JSON
+from ..typings import JSON, ValidDeleteMessageDays
 from .objects import NativeObject
 from .role import Role
 from .user import User
 from .guild import Guild
 
 
-VALID_DELETE_MESSAGE_DAYS = Literal[0, 1, 2, 3, 4, 5, 6, 7]
 
 class Member(NativeObject):
     def __init__(self, connection: Connection, guild: Guild, data: JSON, user: User = None, /) -> None:
@@ -37,5 +36,5 @@ class Member(NativeObject):
     async def kick(self, reason: str, /) -> None:
         return await self._guild.kick(self, reason=reason)
     
-    async def ban(self, *, reason: str = None, delete_message_days: VALID_DELETE_MESSAGE_DAYS) -> None:
+    async def ban(self, *, reason: str = None, delete_message_days: ValidDeleteMessageDays) -> None:
         return await self._guild.ban(self, reason=reason, delete_message_days=delete_message_days)
